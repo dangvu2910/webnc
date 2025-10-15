affa1924218c69f1be16e72ae7c52e4499ab7a5e
sonhai05 <23010007@st.phenikaa-uni.edu.vn>
2025-10-16 00:36:15 +0700

    lsh

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1199,7 +1199,7 @@
                       </td>
                       <td class="px-4 py-3 text-xs">
                         <span
-                          class="px-2 py-1 font-semibold leading-tight text-blue-700 bg-blue-100 rounded-full dark:text-blue-100 dark:bg-blue-700"
+                          class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
                         >
                           Đã xác nhận
                         </span>
